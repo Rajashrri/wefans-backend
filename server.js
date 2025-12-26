af122aaf4e012b30b6d82c083be24a4e5fd3ecbe
Rajashri Chougule <171247501+Rajashrri@users.noreply.github.com>
2025-12-26 11:34:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const errorMiddleware1 = require("./middlewares/error-middleware");
 
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://wefans-front.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true,
 };
